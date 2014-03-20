6cce19e70e05790b35a0f70f00da60c95515e304
mireaulf <mireaulf@>
2014-03-20 15:46:29 -0400

    chore(Constants): add basic constants

---
+++ Constants.py
@@ -0,0 +1,8 @@
+
+class Constants:
+    RANKED_SOLO_5_STRING = 'RANKED_SOLO_5x5'
+    RANKED_TEAM_5_STRING = 'RANKED_TEAM_5x5'
+
+    URL_BASE = 'https://prod.api.pvp.net/api/lol'
+    URL_BASE_STATIC = 'https://prod.api.pvp.net/api/lol/static-data'
+    URL_CHAMPIONS = 'v1/champion'
